a8298729759d755873b298311fe9bf055331b980
jamiul <jamiulewu24@gmail.com>
2023-08-13 07:33:03 +0600

    Design escrow payment option

---
+++ app/Http/Controllers/EscrowPaymentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class EscrowPaymentController extends Controller
+{
+    public function index()
+    {
+        return 'pay by escrow';
+    }
+}
