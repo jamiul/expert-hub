944547e905c993668f0ab57efb41dc22b2e1b726
raihan <raihanulmcse@mail.com>
2023-09-17 10:40:16 +0800

    white-space emmited

--- resources/views/frontend/default/user_sign_up.blade.php
+++ resources/views/frontend/default/user_sign_up.blade.php
@@ -157,13 +157,10 @@
                 $("#reg-form").on("submit", function(evt) {
                     var response = grecaptcha.getResponse();
                     if (response.length == 0) {
-                        //reCaptcha not verified
                         alert("please verify you are humann!");
                         evt.preventDefault();
                         return false;
                     }
-                    //captcha verified
-                    //do the rest of your validations here
                     $("#reg-form").submit();
                 });
             });
