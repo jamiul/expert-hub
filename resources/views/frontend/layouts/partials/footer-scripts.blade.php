bcc63a503ad2c23f83e4aec252be4cce1ad9a18f
Web Developer <webdev@alasala.edu.sa>
2023-10-10 01:17:19 +0500

    expert registration changes

--- resources/views/frontend/layouts/partials/footer-scripts.blade.php
+++ resources/views/frontend/layouts/partials/footer-scripts.blade.php
@@ -1,7 +1,7 @@
 <script src="{{ asset('assets/common/js/vendors.js') }}"></script>
 <script src="{{ asset('assets/common/js/jssocials.min.js') }}"></script>
 <script src="{{ asset('assets/common/js/aiz-core.js') }}"></script>
-<script src="{{ asset('assets/common/js/scripts-custom.js') }}"></script>
+<script src="{{ asset('assets/common/js/scripts-custom.js') }}"></script> 
 
 <script type="text/javascript">
     @foreach (session('flash_notification', collect())->toArray() as $message)
