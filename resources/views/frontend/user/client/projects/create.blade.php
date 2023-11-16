41f3c12d5507bef52c61d356e1127d67ed69088f
Faysal Ahamed <devfaysal@gmail.com>
2023-11-16 13:54:25 +0600

    Create Prodject using Livewire

--- resources/views/frontend/user/client/projects/create.blade.php
+++ resources/views/frontend/user/client/projects/create.blade.php
@@ -2,18 +2,18 @@
 
 @push('top_styles')
     <link rel='stylesheet' href="{{ asset('assets/frontend/css/image-uploader.min.css') }}">
-    <link rel='stylesheet' href="{{ asset('assets/frontend/css/select2.min.css') }}">
+    {{-- <link rel='stylesheet' href="{{ asset('assets/frontend/css/select2.min.css') }}"> --}}
 @endpush
 
 @section('content')
     {{--@include('frontend.user.client.inc.sidebar')--}}
-
-    @include('frontend.user.client.projects.partials._form')
+    @livewire('project.create')
+    {{-- @include('frontend.user.client.projects.partials._form') --}}
 @endsection
 
 @push('bottom_scripts')
-    <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
+    {{-- <script src="https://cdn.jsdelivr.net/bootstrap.tagsinput/0.8.0/bootstrap-tagsinput.min.js"></script>
     <script src="{{ asset('assets/frontend/js/select2.min.js') }}"></script>
-    <script src="{{ asset('assets/frontend/js/image-uploader.min.js') }}"></script>
-    <script src="{{ asset('assets/frontend/js/script.js') }}"></script>
+    <script src="{{ asset('assets/frontend/js/image-uploader.min.js') }}"></script> --}}
+    {{-- <script src="{{ asset('assets/frontend/js/script.js') }}"></script> --}}
 @endpush
