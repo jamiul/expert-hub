826c96f956c473f150be7f37c1c6ae7fdb8e1278
Michael <minhpham1309@gmail.com>
2023-11-17 07:21:47 +0700

    Updated seeder study area

--- database/seeders/StudyAreaTableSeeder.php
+++ database/seeders/StudyAreaTableSeeder.php
@@ -21,21 +21,112 @@ class StudyAreaTableSeeder extends Seeder
             0 =>
             array(
                 'id' => 1,
-                'name' => 'Art',
+                'name' => 'Architecture',
                 'created_at' => '2023-11-09 16:16:12',
                 'updated_at' => '2023-11-09 09:41:13',
             ),
             1 =>
             array(
                 'id' => 2,
-                'name' => 'Architecture',
+                'name' => 'Art',
                 'created_at' => '2023-11-09 16:16:12',
                 'updated_at' => '2023-11-09 09:41:13',
             ),
             2 =>
             array(
                 'id' => 3,
+                'name' => 'Biology',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            3 =>
+            array(
+                'id' => 4,
                 'name' => 'Business',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            4 =>
+            array(
+                'id' => 5,
+                'name' => 'Chemistry',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            5 =>
+            array(
+                'id' => 6,
+                'name' => 'Computer Programming',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            6 =>
+            array(
+                'id' => 7,
+                'name' => 'Data Analysis',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            7 =>
+            array(
+                'id' => 8,
+                'name' => 'Design',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            8 =>
+            array(
+                'id' => 9,
+                'name' => 'Education',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            9 =>
+            array(
+                'id' => 10,
+                'name' => 'Electronics',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            10 =>
+            array(
+                'id' => 11,
+                'name' => 'Engineering',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            11 =>
+            array(
+                'id' => 12,
+                'name' => 'Environmental Science',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            12 =>
+            array(
+                'id' => 13,
+                'name' => 'Nutrition',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            13 =>
+            array(
+                'id' => 14,
+                'name' => 'Healthcare',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            14 =>
+            array(
+                'id' => 15,
+                'name' => 'History',
+                'created_at' => '2023-11-09 16:16:12',
+                'updated_at' => '2023-11-09 09:41:13',
+            ),
+            15 =>
+            array(
+                'id' => 16,
+                'name' => 'Humanities',
                 'created_at' => '2023-11-09 16:16:12',
                 'updated_at' => '2023-11-09 09:41:13',
             ),
