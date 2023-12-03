5149d8b9ad526cf85ab0c4bf3619035c524ce08c
Faysal Ahamed <devfaysal@gmail.com>
2023-12-03 15:24:14 +0600

    wip

---
+++ app/Filament/Resources/AdminResource.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\AdminResource\Pages;
+use App\Filament\Resources\AdminResource\RelationManagers;
+use App\Models\Admin;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Actions\Action;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class AdminResource extends Resource
+{
+    protected static ?string $model = Admin::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Action::make('activities')->url(fn ($record) => AdminResource::getUrl('activities', ['record' => $record]))
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListAdmins::route('/'),
+            'create' => Pages\CreateAdmin::route('/create'),
+            'activities' => Pages\ListAdminActivities::route('/{record}/activities'),
+            'edit' => Pages\EditAdmin::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ app/Filament/Resources/AdminResource/Pages/ListAdminActivities.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\AdminResource\Pages;
+
+use App\Filament\Resources\AdminResource;
+use pxlrbt\FilamentActivityLog\Pages\ListActivities;
+
+class ListAdminActivities extends ListActivities
+{
+    protected static string $resource = AdminResource::class;
+}
