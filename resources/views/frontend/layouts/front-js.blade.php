076b5f21b69d3430d11938e222d32f166d3504dd
Faysal Ahamed <devfaysal@gmail.com>
2023-12-04 14:25:15 +0600

    wip

--- resources/views/frontend/layouts/front-js.blade.php
+++ resources/views/frontend/layouts/front-js.blade.php
@@ -1,4 +1,4 @@
-<script src="https://stg.eduexperthub.com/assets/frontend/js/jquery_3.7.1_jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @vite('resources/js/main.js')
 @vite('resources/js/owl.carousel.min.js')
 @stack('bottom_scripts')
