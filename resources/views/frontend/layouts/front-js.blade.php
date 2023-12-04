1871593fc460a2502ca8d61cf3b3e1c99f0a69ec
Faysal Ahamed <devfaysal@gmail.com>
2023-12-04 12:38:15 +0600

    wip

--- resources/views/frontend/layouts/front-js.blade.php
+++ resources/views/frontend/layouts/front-js.blade.php
@@ -1,4 +1,4 @@
-@vite('resources/js/jquery_3.7.1_jquery.min.js')
+<script src="https://stg.eduexperthub.com/assets/frontend/js/jquery_3.7.1_jquery.min.js"></script>
 @vite('resources/js/main.js')
 @vite('resources/js/owl.carousel.min.js')
 @stack('bottom_scripts')
