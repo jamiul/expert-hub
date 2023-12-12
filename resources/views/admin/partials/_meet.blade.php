4610c0ca1e2beb177d938c31213e77c83676d326
jamiul <jamiulewu24@gmail.com>
2023-12-12 17:12:37 +0600

    Breakdown small pieces admin/about-us page

---
+++ resources/views/admin/partials/_meet.blade.php
@@ -0,0 +1,169 @@
+<div class="academic-seller-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <div class="academic-bar d-flex">
+                    <h3 class="seller-title">Meet Our Experts</h3>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="admin-blog-form-area">
+                    <div class="admin-form-content">
+                        <form action="">
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="col admin-sub-title position-relative">
+                                        <label for="subtitle"
+                                            class="form-label position-absolute level-subtitle">subtitle</label>
+                                        <input id="subtitle" type="text" class="form-control admin-form-input "
+                                            placeholder="Require steps" aria-label="First name">
+                                    </div>
+                                    <div class="col admin-sub-title position-relative">
+                                        <label for="title"
+                                            class="form-label position-absolute level-title">title</label>
+                                        <input id="title" type="text"
+                                            class="form-control admin-form-input admin-m-input"
+                                            placeholder="Search Academic Expert Made Easy" aria-label="Last name">
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <div class="team  team-blog">
+            <div class="team-hire">
+                <div class="row">
+
+                    <div class="col-md-2 col-sm-6">
+                        <div class="team-block"> <a href="#">
+                                <div class="team-img position-relative"> <img
+                                        src=" {{ asset('assets/frontend/img/team-1.png') }}">
+                                    <a class="blog-close position-absolute"> <img class=""
+                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
+                                            alt="close"> </a>
+                                </div>
+                                <div class="hire-text">
+                                    <h4>Darrell Steward</h4>
+                                    <p>Accreditation Specialist</p>
+                                </div>
+                            </a> </div>
+                    </div>
+                    <div class="col-md-2 col-sm-6">
+                        <div class="team-block"> <a href="#">
+                                <div class="team-img position-relative"> <img
+                                        src=" {{ asset('assets/frontend/img/team-2.png') }}">
+                                    <a class="blog-close position-absolute"> <img class=""
+                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
+                                            alt="close"> </a>
+                                </div>
+                                <div class="hire-text">
+                                    <h4>Theresa Webb</h4>
+                                    <p>Industry Expert</p>
+                                </div>
+                            </a> </div>
+                    </div>
+                    <div class="col-md-2 col-sm-6">
+                        <div class="team-block"> <a href="#">
+                                <div class="team-img position-relative"> <img
+                                        src=" {{ asset('assets/frontend/img/team-3.png') }}">
+                                    <a class="blog-close position-absolute"> <img class=""
+                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
+                                            alt="close"> </a>
+                                </div>
+                                <div class="hire-text">
+                                    <h4>Marvin McKinney</h4>
+                                    <p>Policy Expert</p>
+                                </div>
+                            </a> </div>
+                    </div>
+                    <div class="col-md-2 col-sm-6">
+                        <div class="team-block"> <a href="#">
+                                <div class="team-img position-relative"> <img
+                                        src=" {{ asset('assets/frontend/img/team-4.png') }}">
+                                    <a class="blog-close position-absolute"> <img class=""
+                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
+                                            alt="close"> </a>
+                                </div>
+                                <div class="hire-text">
+                                    <h4>Marvin McKinney</h4>
+                                    <p>Curriculum Expert</p>
+                                </div>
+                            </a> </div>
+                    </div>
+                    <div class="col-md-2 col-sm-6">
+                        <div class="team-block"> <a href="#">
+                                <div class="team-img position-relative"> <img
+                                        src=" {{ asset('assets/frontend/img/team-4.png') }}">
+                                    <a class="blog-close position-absolute"> <img class=""
+                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
+                                            alt="close"> </a>
+                                </div>
+                                <div class="hire-text">
+                                    <h4>Marvin McKinney</h4>
+                                    <p>Curriculum Expert</p>
+                                </div>
+                            </a> </div>
+                    </div>
+                    <div class="col-md-2 col-sm-6">
+                        <div class="team-block"> <a href="#">
+                                <div class="team-img position-relative"> <img
+                                        src=" {{ asset('assets/frontend/img/team-3.png') }}">
+                                    <a class="blog-close position-absolute"> <img class=""
+                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
+                                            alt="close"> </a>
+                                </div>
+                                <div class="hire-text">
+                                    <h4>Marvin McKinney</h4>
+                                    <p>Policy Expert</p>
+                                </div>
+                            </a> </div>
+                    </div>
+
+
+                    <div class="row mt-5">
+                        <div class="col-md-2 col-sm-6">
+                            <div class="team-block"> <a href="#">
+                                    <div class="team-img position-relative"> <img
+                                            src=" {{ asset('assets/frontend/img/team-3.png') }}">
+                                        <a class="blog-close position-absolute"> <img class=""
+                                                src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
+                                                alt="close"> </a>
+                                    </div>
+                                    <div class="hire-text">
+                                        <h4>Marvin McKinney</h4>
+                                        <p>Policy Expert</p>
+                                    </div>
+                                </a> </div>
+                        </div>
+                        <div class="col-md-2 col-sm-6">
+                            <div class="team-block">
+                                <div class="academic-single-item d-flex flex-column">
+                                    <div class="admin-add-more"> <a href="#"> <span
+                                                class="material-symbols-outlined" data-bs-toggle="modal"
+                                                data-bs-target="#buildondemand">add</span>
+                                        </a> </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="edux-frontend-left-title">
+                            <button class="save-update">Save Update</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</div>
