f2134f70056c507c93be9fcc9bf26f421f808194
Faysal Ahamed <devfaysal@gmail.com>
2023-12-17 13:05:39 +0600

    wip

--- resources/views/components/form/autocomplete.blade.php
+++ resources/views/components/form/autocomplete.blade.php
@@ -6,7 +6,7 @@
     'readonly' => false,
     'results' => []
 ])
-<div x-data="{ open: false }">
+<div class="autocomplete-field-wrapper" x-data="{ open: false }">
     <div class="form-input-group form-input-has-icon">
         <label class="form-input-label">{{ $label }}</label>
         <input 
@@ -14,14 +14,14 @@
             wire:model.live.debounce.500ms="{{ $name }}" 
             name="{{ $name }}" 
             id="{{ $id }}" 
-            {{ $attributes->merge(['class' => 'form-input-field ' . ($errors->has($name) ? ' has-error':'')]) }} 
+            {{ $attributes->merge(['class' => 'form-input-field ']) }} 
         >
         <span class="form-input-icon"><x-icon.search fill="#ccc"/></span>
         @error($name)
             <div class="error-message">{{ $message }}</div>
         @enderror
     </div>
-    <div>
+    <div class="autocomplete-field-suggestion">
         <ul class="shadow py-2" x-show="open" x-on:click.outside="open = false" x-on:keyup.escape.window="open = false">
             @forelse ($results as $result)
                 <li wire:key="{{ $result->id }}" x-on:click="open = false; $wire.select{{ ucfirst($name) }}('{{ $result->name }}')"> {{ $result->name }} </li>
