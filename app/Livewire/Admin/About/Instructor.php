c6f5b85a3bdfc97f75166c1d9c2d86482b9b90fc
jamiul <jamiulewu24@gmail.com>
2023-12-20 21:35:35 +0600

    [ABOUT-US] WIP Meet our expert

--- app/Livewire/Admin/About/Instructor.php
+++ app/Livewire/Admin/About/Instructor.php
@@ -2,12 +2,57 @@
 
 namespace App\Livewire\Admin\About;
 
+use App\Models\User;
+use App\Models\AboutUs;
 use Livewire\Component;
 
 class Instructor extends Component
 {
+    public $instructor_title, $instructor_subtitle, $instructor_list;
+    public $instructors, $instructorPage, $expertId;
+
+    public function rules()
+    {
+        return [
+            'instructor_title' => ['required','string'],
+            'instructor_subtitle' => ['required','string'],
+            'instructor_list' => ['array'],
+        ];
+    }
+
+    public function mount()
+    {
+        $this->instructorPage = AboutUs::first();
+        if (is_null($this->instructorPage)) {
+            $this->instructorPage = AboutUs::create();
+        }
+        $this->instructor_title = $this->instructorPage->instructor_title;
+        $this->instructor_subtitle = $this->instructorPage->instructor_subtitle;
+        $this->instructor_list = $this->instructorPage->instructor_list;
+    }
+
+    public function addExpert($id)
+    {
+        $this->expertId = $id;
+        array_push($this->instructor_list, $this->expertId);
+    }
+
+    public function saveExpert()
+    {
+        $data = $this->validate();
+
+        $this->instructorPage->update([
+            'instructor_title' => $data['instructor_title'],
+            'instructor_subtitle' => $data['instructor_subtitle'],
+            'instructor_list' => $this->instructor_list,
+        ]);
+
+        return redirect()->to('/admin/about-us');
+    }
+
     public function render()
     {
+        $this->instructors = User::where('active_profile', 'Expert')->get();
         return view('livewire.admin.about.instructor');
     }
 }
--- resources/views/livewire/admin/about/instructor.blade.php
+++ resources/views/livewire/admin/about/instructor.blade.php
@@ -8,27 +8,33 @@
                 </div>
             </div>
         </div>
-
         <div class="row">
             <div class="col-md-12">
                 <div class="admin-blog-form-area">
                     <div class="admin-form-content">
-                        <form action="">
+                        <form wire:submit="saveExpert">
                             <div class="col-md-12">
                                 <div class="row">
                                     <div class="col admin-sub-title position-relative">
                                         <label for="subtitle"
                                             class="form-label position-absolute level-subtitle">subtitle</label>
                                         <input id="subtitle" type="text" class="form-control admin-form-input "
-                                            placeholder="Require steps" aria-label="First name">
+                                            placeholder="Require steps" aria-label="First name"
+                                            wire:model="instructor_subtitle">
                                     </div>
                                     <div class="col admin-sub-title position-relative">
                                         <label for="title"
                                             class="form-label position-absolute level-title">title</label>
                                         <input id="title" type="text"
                                             class="form-control admin-form-input admin-m-input"
-                                            placeholder="Search Academic Expert Made Easy" aria-label="Last name">
+                                            placeholder="Search Academic Expert Made Easy" aria-label="Last name"
+                                            wire:model="instructor_title">
                                     </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="edux-frontend-left-title">
+                                    <button class="save-update">Save Update</button>
                                 </div>
                             </div>
                         </form>
@@ -40,94 +46,29 @@
         <div class="team  team-blog">
             <div class="team-hire">
                 <div class="row">
-                    <div class="col-md-2 col-sm-6">
-                        <div class="team-block"> <a href="#">
-                                <div class="team-img position-relative"> <img
-                                        src=" {{ asset('assets/frontend/img/team-1.png') }}">
-                                    <a class="blog-close position-absolute"> <img class=""
-                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
-                                            alt="close"> </a>
-                                </div>
-                                <div class="hire-text">
-                                    <h4>Darrell Steward</h4>
-                                    <p>Accreditation Specialist</p>
-                                </div>
-                            </a> </div>
-                    </div>
-                    <div class="col-md-2 col-sm-6">
-                        <div class="team-block"> <a href="#">
-                                <div class="team-img position-relative"> <img
-                                        src=" {{ asset('assets/frontend/img/team-2.png') }}">
-                                    <a class="blog-close position-absolute"> <img class=""
-                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
-                                            alt="close"> </a>
-                                </div>
-                                <div class="hire-text">
-                                    <h4>Theresa Webb</h4>
-                                    <p>Industry Expert</p>
-                                </div>
-                            </a> </div>
-                    </div>
-                    <div class="col-md-2 col-sm-6">
-                        <div class="team-block"> <a href="#">
-                                <div class="team-img position-relative"> <img
-                                        src=" {{ asset('assets/frontend/img/team-3.png') }}">
-                                    <a class="blog-close position-absolute"> <img class=""
-                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
-                                            alt="close"> </a>
-                                </div>
-                                <div class="hire-text">
-                                    <h4>Marvin McKinney</h4>
-                                    <p>Policy Expert</p>
-                                </div>
-                            </a> </div>
-                    </div>
-                    <div class="col-md-2 col-sm-6">
-                        <div class="team-block"> <a href="#">
-                                <div class="team-img position-relative"> <img
-                                        src=" {{ asset('assets/frontend/img/team-4.png') }}">
-                                    <a class="blog-close position-absolute"> <img class=""
-                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
-                                            alt="close"> </a>
-                                </div>
-                                <div class="hire-text">
-                                    <h4>Marvin McKinney</h4>
-                                    <p>Curriculum Expert</p>
-                                </div>
-                            </a> </div>
-                    </div>
-                    <div class="col-md-2 col-sm-6">
-                        <div class="team-block"> <a href="#">
-                                <div class="team-img position-relative"> <img
-                                        src=" {{ asset('assets/frontend/img/team-4.png') }}">
-                                    <a class="blog-close position-absolute"> <img class=""
-                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
-                                            alt="close"> </a>
-                                </div>
-                                <div class="hire-text">
-                                    <h4>Marvin McKinney</h4>
-                                    <p>Curriculum Expert</p>
-                                </div>
-                            </a> </div>
-                    </div>
-                    <div class="col-md-2 col-sm-6">
-                        <div class="team-block"> <a href="#">
-                                <div class="team-img position-relative"> <img
-                                        src=" {{ asset('assets/frontend/img/team-3.png') }}">
-                                    <a class="blog-close position-absolute"> <img class=""
-                                            src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
-                                            alt="close"> </a>
-                                </div>
-                                <div class="hire-text">
-                                    <h4>Marvin McKinney</h4>
-                                    <p>Policy Expert</p>
-                                </div>
-                            </a> </div>
-                    </div>
 
-
+                    @if (count($instructor_list) > 0)
+                        @foreach ($instructor_list as $instructor)
+                            <div class="col-md-2 col-sm-6">
+                                <div class="team-block">
+                                    <a href="#">
+                                        <div class="team-img position-relative"> <img
+                                                src=" {{ asset('assets/frontend/img/team-1.png') }}">
+                                            <a class="blog-close position-absolute"> <img class=""
+                                                    src="{{ asset('assets/frontend/img/blog-admin/blog-close.png') }}"
+                                                    alt="close"> </a>
+                                        </div>
+                                        <div class="hire-text">
+                                            <h4>{{ getFullNameByExpertID($instructor) }}</h4>
+                                            <p>Accreditation Specialist</p>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        @endforeach
+                    @endif
                     <div class="row mt-5">
-                        <div class="col-md-2 col-sm-6">
+                        {{-- <div class="col-md-2 col-sm-6">
                             <div class="team-block"> <a href="#">
                                     <div class="team-img position-relative"> <img
                                             src=" {{ asset('assets/frontend/img/team-3.png') }}">
@@ -140,7 +81,7 @@
                                         <p>Policy Expert</p>
                                     </div>
                                 </a> </div>
-                        </div>
+                        </div> --}}
                         <div class="col-md-2 col-sm-6">
                             <div class="team-block">
                                 <div class="academic-single-item d-flex flex-column">
@@ -152,13 +93,6 @@
                             </div>
                         </div>
                     </div>
-
-                    <div class="row">
-                        <div class="edux-frontend-left-title">
-                            <button class="save-update">Save Update</button>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
