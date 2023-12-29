fc39bc83b12486ee53d37a9643abac0ab5528efe
Faysal Ahamed <devfaysal@gmail.com>
2023-12-29 20:51:21 +0600

    Favourite functionality for Scholarship

--- app/Models/Scholarship.php
+++ app/Models/Scholarship.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Enums\Scholarship\StudentType;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Scholarship extends Model
@@ -16,6 +17,28 @@ class Scholarship extends Model
         'deadline' => 'date',
         'student_type' => StudentType::class,
     ];
+
+    public function favourites(): MorphMany
+    {
+        return $this->morphMany(Favourite::class, 'loveable');
+    }
+
+    public function favourite()
+    {
+        $attributes = ['profile_id' => auth()->user()->profile->id];
+        if(!$this->favourites()->where($attributes)->exists()){
+            $this->favourites()->create($attributes);
+        }
+    }
+
+    public function favourited()
+    {
+        if(!auth()->user()){
+            return null;
+        }
+        $attributes = ['profile_id' => auth()->user()->profile->id];
+        return $this->favourites()->where($attributes)->exists();
+    }
 
     public function university()
     {
