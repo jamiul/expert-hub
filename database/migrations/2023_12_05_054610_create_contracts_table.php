177c205033d15af15a83e63e971d77f92e3c4d5f
Faysal Ahamed <devfaysal@gmail.com>
2024-01-03 14:19:30 +0600

    Project tables and message tables

---
+++ database/migrations/2023_12_05_054610_create_contracts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contracts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('project_id');
+            $table->foreignId('expert_id');
+            $table->text('message');
+            $table->string('status');
+            $table->boolean('active')->default(1);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contracts');
+    }
+};
