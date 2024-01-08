518f091afe92625db5a301563eb4f92e64d24ec2
alamrb <alamrb2023@gmail.com>
2024-01-08 11:46:55 +0600

    find-experts slider updated

--- resources/views/frontend/expert/partials/_slider.blade.php
+++ resources/views/frontend/expert/partials/_slider.blade.php
@@ -2,59 +2,93 @@
     <div class="container">
         <div class="heading">
             <h4>Instructor</h4>
-            <h2>Meet Our Experts for Consultation</h2>
+            <h2>Meet Our Experts</h2>
         </div>
         <div id="owl-carousel" class="owl-carousel owl-theme">
             <div class="item">
-                <div class="carousel-img">
-                    <img src="{{ asset('assets/frontend/img/find-trad1.png') }}">
-                </div>
-                <div class="carousel-text">
-                    <h4>Darrell Steward</h4>
-                    <p>Accreditation Specialist</p>
-                    <h5>Oxford University</h5>
-                </div>
+
+            <div class="single-team-member">
+            <div class="single-person-deniyel">
+              <img src="{{ asset('assets/frontend/img/img6.png') }}" alt="person">
             </div>
+            <div class="person-details">
+              <h3>Theresa Webb</h3>
+              <p class="mb-0">Agricultural Policy Expert</p>
+              <a href="#">Harvard University</a>
+            </div>
+          </div>
+            </div>
+
             <div class="item">
-                <div class="carousel-img">
-                    <img src="{{ asset('assets/frontend/img/find-trad2.png') }}">
+                <div class="single-team-member">
+                <div class="single-person-deniyel">
+                <img src="{{ asset('assets/frontend/img/img5.png') }}" alt="person">
                 </div>
-                <div class="carousel-text">
-                    <h4>Theresa Webb</h4>
-                    <p>Industry Expert</p>
-                    <h5>Harvard University</h5>
+                <div class="person-details">
+                <h3>Marvin McKinney</h3>
+                <p class="mb-0">Architectural Design Expert</p>
+                <a href="#">Oxford University</a>
                 </div>
             </div>
+            </div>
+
             <div class="item">
-                <div class="carousel-img">
-                    <img src="{{ asset('assets/frontend/img/find-trad3.png') }}">
+                <div class="single-team-member">
+                <div class="single-person-deniyel">
+                <img src="{{ asset('assets/frontend/img/team-5.png') }}" alt="person">
                 </div>
-                <div class="carousel-text">
-                    <h4>Marvin McKinney</h4>
-                    <p>Policy Expert</p>
-                    <h5>Oxford University</h5>
+                <div class="person-details">
+                <h3>Marvin McKinney</h3>
+                <p class="mb-0">Public Health Manager</p>
+                <a href="#">Oxford University</a>
                 </div>
             </div>
+            </div>
+
             <div class="item">
-                <div class="carousel-img">
-                    <img src="{{ asset('assets/frontend/img/find-trad3.png') }}">
+                <div class="single-team-member">
+                <div class="single-person-deniyel">
+                <img src="{{ asset('assets/frontend/img/img8.png') }}" alt="person">
                 </div>
-                <div class="carousel-text">
-                    <h4>Marvin McKinney</h4>
-                    <p>Policy Expert</p>
-                    <h5>Oxford University</h5>
+                <div class="person-details">
+                <h3>Civil Law Expert</h3>
+                <p class="mb-0">Accreditation Specialist</p>
+                <a href="#">Oxford University</a>
                 </div>
             </div>
+            </div>
+
             <div class="item">
-                <div class="carousel-img">
-                    <img src="{{ asset('assets/frontend/img/find-trad3.png') }}">
+                <div class="single-team-member">
+                <div class="single-person-deniyel">
+                <img src="{{ asset('assets/frontend/img/team-4.png') }}" alt="person">
                 </div>
-                <div class="carousel-text">
-                    <h4>Marvin McKinney</h4>
-                    <p>Policy Expert</p>
-                    <h5>Oxford University</h5>
+                <div class="person-details">
+                <h3>Darrell Steward</h3>
+                <p class="mb-0">Accreditation Specialist</p>
+                <a href="#">Oxford University</a>
                 </div>
             </div>
+            </div>
+
+            <div class="item">
+                <div class="single-team-member">
+                <div class="single-person-deniyel">
+                <img src="{{ asset('assets/frontend/img/img6.png') }}" alt="person">
+                </div>
+                <div class="person-details">
+                <h3>Darrell Steward</h3>
+                <p>Accreditation Specialist</p>
+                </div>
+            </div>
+            </div>
+
+
+
+        </div>
+
+        <div class="row justify-content-center mb-4">
+            <button type="button" class="btn btn-primary edux-btn-primary">View All</button>
         </div>
     </div>
 </div>
