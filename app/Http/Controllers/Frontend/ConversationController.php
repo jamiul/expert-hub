3760f21f4b7cefbd8c450585d1a079b93d16b86b
Neloy Ahmed <rubelinvisible@gmail.com>
2024-01-10 12:40:35 +0600

    Sotre a new conversation

--- app/Http/Controllers/Frontend/ConversationController.php
+++ app/Http/Controllers/Frontend/ConversationController.php
@@ -2,23 +2,39 @@
 
 namespace App\Http\Controllers\Frontend;
 
+use App\Events\ConversationCreated;
 use App\Models\Conversation;
+use App\Models\Message;
+use App\Models\Participant;
+use Auth;
 use Illuminate\Http\Request;
 
 class ConversationController extends Controller
 {
                 
         public function store()
-        {
+        {   
+            $conversationCreator = Auth::user();
+            $participantProfileIDs = [$conversationCreator->profile->id, 2]; //TODO:receive the second user from UI
 
-            $conversation = Conversation::create(['title' => 'Neloy', 'profile_id' => 1]);
-        
-            // $users = collect(request('users'));
-            // $users->push(auth()->user()->id);
-        
-            // $group->users()->attach($users);
+            $conversation = Conversation::create(['profile_id' => $conversationCreator->profile->id, 'title' => $conversationCreator->first_name]);
+            
+            foreach($participantProfileIDs as $participantProfileID) {
+                Participant::create(['conversation_id' => $conversation->id, 'profile_id' => $participantProfileID]);
+            }
+
+            //TODO: receive `content` from UI
+            Message::create(['conversation_id' => $conversation->id, 'profile_id' => $conversationCreator->profile->id, 
+            'content' => "I want to discuss with you about a project"]);
+            
+            // ConversationCreated::dispatch();
         
             return $conversation;
+        }
+
+        public function fetch()
+        {
+            
         }
     
 }
