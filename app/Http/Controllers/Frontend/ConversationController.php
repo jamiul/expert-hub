07ababacb95fe5a2e3c29b76ae3660e870367567
Neloy Ahmed <rubelinvisible@gmail.com>
2024-01-17 23:34:24 +0600

    Add DB trnsaction in ConversationController->store() method

--- app/Http/Controllers/Frontend/ConversationController.php
+++ app/Http/Controllers/Frontend/ConversationController.php
@@ -9,6 +9,7 @@ use App\Models\MessageRecipient;
 use App\Models\Participant;
 use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ConversationController extends Controller
 {
@@ -17,30 +18,33 @@ class ConversationController extends Controller
     public function index()
     {
         // $currentUsersConversations = Participant::with('conversation.messages')->where('profile_id', Auth::user()->profile->id)->get();
-        
+
         return view('frontend.messaging.index');
     }
 
     public function store()
-    {
-        $conversationCreator = Auth::user();
-        $participantProfileIDs = [$conversationCreator->profile->id, 2]; //TODO:receive the second user from UI
+    {        
+        DB::transaction(function () {
+            $authUser = Auth::user();
+            $participantProfileIDs = [$authUser->profile->id, 2]; //TODO:receive the second user from UI
 
-        $conversation = Conversation::create(['creator_profile_id' => $conversationCreator->profile->id, 'title' => $conversationCreator->first_name]);
+            $conversation = Conversation::create(['creator_profile_id' => $authUser->profile->id, 'title' => $authUser->first_name]);
 
-        foreach ($participantProfileIDs as $participantProfileID) {
-            Participant::create(['conversation_id' => $conversation->id, 'profile_id' => $participantProfileID]);
-        }
+            foreach ($participantProfileIDs as $participantProfileID) {
+                Participant::create(['conversation_id' => $conversation->id, 'profile_id' => $participantProfileID]);
+            }
 
-        //TODO: receive `content` from UI
-        $message = Message::create([
-            'conversation_id' => $conversation->id, 'creator_profile_id' => $conversationCreator->profile->id,
-            'content' => "I want to discuss with you about a project"
-        ]);
+            //TODO: receive `content` from UI
+            $message = Message::create([
+                'conversation_id' => $conversation->id, 'sender_profile_id' => $authUser->profile->id,
+                'content' => "I want to discuss with you about a project"
+            ]);
 
-        MessageRecipient::create(['message_id' => $message->id, 'recipient_profile_id' => 2]); //TODO:take participant profile id dynamically
+            MessageRecipient::create(['message_id' => $message->id, 'recipient_profile_id' => 2]); //TODO:take participant profile id dynamically
+        });
+
         // ConversationCreated::dispatch();
 
-        return $conversation;
+        
     }
 }
