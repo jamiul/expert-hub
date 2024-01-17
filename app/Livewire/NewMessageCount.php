de03f924830da741c9a76d1146ca3b077502c77e
Neloy Ahmed <rubelinvisible@gmail.com>
2024-01-17 23:41:47 +0600

    dispatch show-new-message event when new messaged is clicked and modify  pulling logic

--- app/Livewire/NewMessageCount.php
+++ app/Livewire/NewMessageCount.php
@@ -11,13 +11,14 @@ class NewMessageCount extends Component
     public function markAsSeen($id)
     {
         MessageRecipient::where('id', $id)->update(['seen_at' => Carbon::now()]);
+        $this->dispatch('show-new-message', $id);
 
-        return redirect('messaging');
+        // return redirect('messaging');
     }
 
     public function render()
     {
-        $unreadMessages = MessageRecipient::with('message')->whereNull('seen_at')->where('recipient_profile_id', 2)->get();
+        $unreadMessages = MessageRecipient::with('message')->whereNull('seen_at')->where('recipient_profile_id', auth()->user()->profile->id)->get();
         $unreadMessageCount = MessageRecipient::whereNull('seen_at')->where('recipient_profile_id', '=', auth()->user()->profile->id)->count();
         
         return view('livewire.new-message-count', compact('unreadMessages', 'unreadMessageCount'));
