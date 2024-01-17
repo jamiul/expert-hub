4cd370042dfd16b0c54794ea6a066dd7e3979548
Faysal Ahamed <devfaysal@gmail.com>
2024-01-17 16:36:51 +0600

    Added table name to model

--- app/Models/ExpertKYC.php
+++ app/Models/ExpertKYC.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
-class ExpertKYC extends Model implements HasMedia {
-    use SoftDeletes, InteractsWithMedia;
+class ExpertKYC extends Model implements HasMedia 
+{
+    use SoftDeletes;
+    use InteractsWithMedia;
+
+    protected $table = 'expert_kyc';
 
     protected $guarded = [];
 
