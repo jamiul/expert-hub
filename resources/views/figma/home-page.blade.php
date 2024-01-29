d6e956608affa896b4a3dec0cad46fb6f0601f4e
alamrb <alamrb2023@gmail.com>
2024-01-29 07:35:25 +0600

    home page bottom Part Updated

--- resources/views/figma/home-page.blade.php
+++ resources/views/figma/home-page.blade.php
@@ -983,18 +983,18 @@
           <h2>Discover Our Expertise</h2>
         </div>
         <div class="tabbable-panel">
-          <ul class="nav nav-tabs" id="myTab1" role="tablist">
+          <!-- <ul class="nav nav-tabs" id="myTab1" role="tablist">
             <li class="nav-item" role="presentation">
               <button class="nav-link active" id="home-tab1" data-bs-toggle="tab" data-bs-target="#home1" type="button" role="tab" aria-controls="home" aria-selected="true">For Clients</button>
             </li>
             <li class="nav-item" role="presentation">
               <button class="nav-link" id="profile-tab1" data-bs-toggle="tab" data-bs-target="#profile1" type="button" role="tab" aria-controls="profile" aria-selected="false">For Experts</button>
             </li>
-          </ul>
+          </ul> -->
           <div class="tab-content" id="myTabContent">
             <div class="tab-pane fade show active" id="home1" role="tabpanel" aria-labelledby="home-tab">
               <div class="row">
-                <div class="col-md-3 col-sm-6">
+                <div class="col-md-6 col-sm-6">
                   <div class="accordion">
                     <div class="at-item">
                       <div class="at-title active">
@@ -1136,7 +1136,7 @@
                     </div>
                   </div>
                 </div>
-                <div class="col-md-3 col-sm-6">
+                <div class="col-md-6 col-sm-6">
                   <div class="accordion">
                     <div class="at-item">
                       <div class="at-title">
@@ -1301,316 +1301,9 @@
                     </div>
                   </div>
                 </div>
-                <div class="col-md-3 col-sm-6">
-                  <div class="accordion">
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Engineering</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Social Science</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Environmental Science</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Nutrition</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Healthcare</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>History</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Science</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="col-md-3 col-sm-6">
-                  <div class="accordion">
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Humanities</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Languages</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Law</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Literature</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Mathematics</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                    <div class="at-item">
-                      <div class="at-title">
-                        <h2>Medicine</h2>
-                      </div>
-                      <div class="at-tab">
-                        <ul>
-                          <li><a href="#">Architecture History</a></li>
-                          <li><a href="#">Architecture Restoration</a></li>
-                          <li><a href="#">Construction</a></li>
-                          <li><a href="#">Dynamics</a></li>
-                          <li><a href="#">History</a></li>
-                          <li><a href="#">Housing</a></li>
-                          <li><a href="#">Infrastructure</a></li>
-                          <li><a href="#">Physics</a></li>
-                          <li><a href="#">Railway Engeering</a></li>
-                          <li><a href="#">Religion</a></li>
-                          <li><a href="#">Steel</a></li>
-                          <li><a href="#">Transportation</a></li>
-                          <li><a href="#">Vernacular Architecture</a></li>
-                          <li><a href="#">World History</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  </div>
-                </div>
               </div>
             </div>
-            <div class="tab-pane fade" id="profile1" role="tabpanel" aria-labelledby="profile-tab">
+            <!-- <div class="tab-pane fade" id="profile1" role="tabpanel" aria-labelledby="profile-tab">
               <div class="row">
                 <div class="col-md-3 col-sm-6">
                   <div class="accordion">
@@ -2108,7 +1801,7 @@
                   </div>
                 </div>
               </div>
-            </div>
+            </div> -->
           </div>
         </div>
       </div>
