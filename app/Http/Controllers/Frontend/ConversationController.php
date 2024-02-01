8f0357260cfc00a4cca821cb533379f8735f42c8
Neloy Ahmed <rubelinvisible@gmail.com>
2024-02-01 12:04:42 +0600

    issue fix: messaged is delivering to other user than recipients during converstion creation

--- app/Http/Controllers/Frontend/ConversationController.php
+++ app/Http/Controllers/Frontend/ConversationController.php
@@ -61,7 +61,7 @@ class ConversationController extends Controller
                 'content' => "I want to discuss with you about a project"
             ]);
 
-            MessageRecipient::create(['conversation_id' => $conversation->id, 'message_id' => $message->id, 'recipient_profile_id' => 2]); //TODO:take participant profile id dynamically
+            MessageRecipient::create(['conversation_id' => $conversation->id, 'message_id' => $message->id, 'recipient_profile_id' => $id]); //TODO:take participant profile id dynamically
         });
 
         // ConversationCreated::dispatch();
