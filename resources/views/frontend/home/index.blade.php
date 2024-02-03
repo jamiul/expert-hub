25f61dcd67cd3ba2194eb939ccc5ff0f8da48c3d
Faysal Ahamed <devfaysal@gmail.com>
2024-02-03 11:21:16 +0600

    Home page fix

--- resources/views/frontend/home/index.blade.php
+++ resources/views/frontend/home/index.blade.php
@@ -2,12 +2,68 @@
 
 @section('content')
     @include('frontend.home.partials._product_slider')
-    <livewire:require-steps :title="$requireTitle">
+    <div class="page-section work-steps-section">
+        <div class="container">
+            <div style="max-width: 725px; margin-bottom: 75px" class="section-title-wrapper text-center mx-auto">
+                <h2 class="section-title">Required Steps</h2>
+                <p class="section-title-lead">Navigating the ExpertGate Platform</p>
+                <p class="section-title-summary">Step into the largest global network of academic and industry experts, ready to tackle your most complex projects. Find the perfect expert for your project with our simple, four-step process.</p>
+            </div>
+            <div class="work-steps-wrapper">
+                <div class="work-steps-box">
+                    <div class="work-steps-icon">
+                        <span>1</span>
+                        <img src="<?php echo e(asset('assets/frontend/img/step-post-project.svg')); ?>">
+                    </div>
+                    <div class="work-steps-content">
+                        <h3>Post Your Project</h3>
+                        <p>Whether it's a university, organisational or personal project, our academic and industry experts are here to help.</p>
+                    </div>
+                </div> <!--work-steps-box-->
+                <div class="work-steps-box">
+                    <div class="work-steps-icon">
+                        <span>2</span>
+                        <img src="<?php echo e(asset('assets/frontend/img/step-interest.svg')); ?>">
+                    </div>
+                    <div class="work-steps-content">
+                        <h3>Expressions of Interest</h3>
+                        <p>Highly-qualified academic and industry experts will express interest if they are available to contribute to your project.</p>
+                    </div>
+                </div> <!--work-steps-box-->
+                <div class="work-steps-box">
+                    <div class="work-steps-icon">
+                        <span>3</span>
+                        <img src="<?php echo e(asset('assets/frontend/img/step-perfect-match.svg')); ?>">
+                    </div>
+                    <div class="work-steps-content">
+                        <h3>Choose Your Perfect Match</h3>
+                        <p>Make an informed choice by selecting the academic or industry expert who aligns best with your project requirements.</p>
+                    </div>
+                </div> <!--work-steps-box-->
+                <div class="work-steps-box">
+                    <div class="work-steps-icon">
+                        <span>4</span>
+                        <img src="<?php echo e(asset('assets/frontend/img/step-pay.svg')); ?>">
+                    </div>
+                    <div class="work-steps-content">
+                        <h3 style="max-width: 120px">Pay Securely</h3>
+                        <p>Book with confidence as your payment is held securely in escrow until the project is completed to your satisfaction.</p>
+                    </div>
+                </div> <!--work-steps-box-->
+            </div>
+            <div style="margin-top: 36px" class="text-center">
+                <a href="{{ route('auth.registration') }}" class="btn btn-primary btn-md" style="width: 218px">Get Started</a>
+            </div>
+        </div>
+    </div>
+
     @include('frontend.home.partials._categories_main')
-    @include('frontend.home.partials._team')
-    @include('frontend.home.partials._eduexhub')
+    <livewire:about.team />
+    <livewire:about.apart-set :title="$aboutUsDTO->apart_title" :subtitle="$aboutUsDTO->apart_subtitle"/>
     @include('frontend.home.partials._academic_block')
-    @include('frontend.home.partials._scholarship')
+    <div class="scholarship">
+        @livewire('home.scholarship-list')
+    </div>
     @livewire('skill-sets')
 @endsection
 
