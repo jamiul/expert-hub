4c2a1dfa83181e2098b6f59467040e71e44e9e80
shamimhasan <shamimhasan524@yahoo.com>
2024-02-08 07:47:46 +0600

    update expert payment

--- app/Enums/TransactionType.php
+++ app/Enums/TransactionType.php
@@ -5,8 +5,11 @@ namespace App\Enums;
 enum TransactionType: string
 {
     case Payment = 'Payment';
+
     case FixedPrice = 'Fixed Price';
+
     case ServiceFee = 'Service Fee';
+
     case ContractInitializationFee = 'Contract Initialization Fee';
 
     case GST = 'GST';
