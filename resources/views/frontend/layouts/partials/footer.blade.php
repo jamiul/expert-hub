95e7bd9f505428609a65a744a7213ef84ae49450
shamimhasan <shamimhasan524@yahoo.com>
2024-02-10 08:54:18 +0600

    update payment system

--- resources/views/frontend/layouts/partials/footer.blade.php
+++ resources/views/frontend/layouts/partials/footer.blade.php
@@ -77,7 +77,7 @@
             <div class="row">
                 <div class="col-md-6 col-sm-6">
                     <div class="footer-left">
-                        <p>© {{ date('Y') }} Sydney IBS Pty Ltd</p>
+                        <p>© {{ date('Y') }} Sydney IBS Pty Ltd v{{ env('VERSION') }}</p>
                     </div>
                 </div>
                 <div class="col-md-6 col-sm-6">
