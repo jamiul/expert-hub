040ec11bc7488f1f69214f33a8bf792899452939
Abdur Rahman Suzon <arsuzon.khan@gmail.com>
2024-02-18 08:57:20 +0600

    Suzon/Comment Old CSS

--- resources/views/livewire/skill-sets.blade.php
+++ resources/views/livewire/skill-sets.blade.php
@@ -1,4 +1,4 @@
-<div class="page-section skill-set-section">
+<div class="page-section category-accordion-section">
     <div class="container">
         <div style="margin-bottom:34px; max-width: 700px" class="section-title-wrapper text-center mx-auto">
             <h2 class="section-title">Experts</h2>
@@ -7,12 +7,14 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="category-accordion">
+                    <div>
                     @foreach ($expertFields->take($perRowExpert) as $expertField)
-                        <div class="category-accordion-item">
-                            <div class="category-accordion-title">
+                        <div class="category-accordion-item" x-data="{ isOpen: false }"
+                             :class="{ 'accordion-item-active': isOpen }">
+                            <div class="category-accordion-title" x-on:click="isOpen = !isOpen">
                                 <h3>{{ $expertField->name }}</h3>
                             </div>
-                            <div class="category-accordion-content">
+                            <div class="category-accordion-content" x-show="isOpen">
                                 <ul>
                                     @foreach ($expertField->children->sortBy('name') as $child)
                                         <li>
@@ -23,12 +25,15 @@
                             </div>
                         </div>
                     @endforeach
+                    </div>
+                    <div>
                     @foreach ($expertFields->skip($perRowExpert)->take($perRowExpert) as $expertField)
-                        <div class="at-item">
-                            <div class="at-title">
+                        <div class="category-accordion-item" x-data="{ isOpen: false }"
+                             :class="{ 'accordion-item-active': isOpen }">
+                            <div class="category-accordion-title" x-on:click="isOpen = !isOpen">
                                 <h3>{{ $expertField->name }}</h3>
                             </div>
-                            <div class="at-tab">
+                            <div class="category-accordion-content" x-show="isOpen">
                                 <ul>
                                     @foreach ($expertField->children->sortBy('name') as $child)
                                         <li>
@@ -39,6 +44,7 @@
                             </div>
                         </div>
                     @endforeach
+                    </div>
                 </div>
             </div>
 
