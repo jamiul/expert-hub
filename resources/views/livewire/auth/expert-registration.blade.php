d4e5a8835b924605a5b5a45a3cde19b2265343e6
Abdur Rahman Suzon <arsuzon.khan@gmail.com>
2024-02-19 06:41:33 +0600

    Log in page ui Update

--- resources/views/livewire/auth/expert-registration.blade.php
+++ resources/views/livewire/auth/expert-registration.blade.php
@@ -1,4 +1,4 @@
-<section class="profile-setup">
+<section class="page-section-sm profile-setup">
     <div class="container">
         <div class="row">
             <div class="col-xl-3 col-lg-4 col-md-4">
