71feb107a1c6ac687ab729e6ccd4fe6da529657f
shamimhasan <shamimhasan524@yahoo.com>
2024-02-27 01:18:43 +0600

    Social Authentication System

--- app/Http/Controllers/Frontend/Auth/SocialLoginController.php
+++ app/Http/Controllers/Frontend/Auth/SocialLoginController.php
@@ -2,10 +2,14 @@
 
 namespace App\Http\Controllers\Frontend\Auth;
 
+use App\Enums\ProfileStatus;
 use App\Enums\ProfileType;
 use App\Http\Controllers\Controller;
+use App\Models\Profile;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 use Auth;
 
@@ -19,13 +23,25 @@ class SocialLoginController extends Controller
         $linkedin_user = Socialite::driver('linkedin-openid')->user();
         dd($linkedin_user);
 
+        $profileType = ProfileType::Client;
+
         $user = User::updateOrCreate([
             'linkedin_id' => $linkedin_user->id,
         ], [
-            'name' => $linkedin_user->name,
-            'email' => $linkedin_user->email,
+            'name' => $facebook_user->name,
+            'email' => $facebook_user->email,
+            'username' => Str::uuid(),
+            'password' => Hash::make(Str::password(8)),
+            'active_profile' => $profileType,
             'linkedin_token' => $linkedin_user->token,
             'linkedin_refresh_token' => $linkedin_user->refreshToken,
+        ]);
+
+        Profile::updateOrCreate([
+            'user_id' => $user->id
+        ], [
+            'type'    => $profileType,
+            'status'  => ProfileStatus::Draft,
         ]);
 
         Auth::login($user);
@@ -44,13 +60,25 @@ class SocialLoginController extends Controller
         $facebook_user = Socialite::driver('facebook')->user();
         dd($facebook_user);
 
+        $profileType = ProfileType::Client;
+
         $user = User::updateOrCreate([
             'facebook_id' => $facebook_user->id,
         ], [
             'name' => $facebook_user->name,
             'email' => $facebook_user->email,
+            'username' => Str::uuid(),
+            'password' => Hash::make(Str::password(8)),
+            'active_profile' => $profileType,
             'facebook_token' => $facebook_user->token,
             'facebook_refresh_token' => $facebook_user->refreshToken,
+        ]);
+
+        Profile::updateOrCreate([
+            'user_id' => $user->id
+        ], [
+            'type'    => $profileType,
+            'status'  => ProfileStatus::Draft,
         ]);
 
         Auth::login($user);
