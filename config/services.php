ae1fb429d381ac0e9b10f5d2b70bdecdc3a62d21
shamimhasan <shamimhasan524@yahoo.com>
2024-02-27 00:59:41 +0600

    update payment system

--- config/services.php
+++ config/services.php
@@ -31,4 +31,16 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'facebook' => [
+        'client_id' => env('FACEBOOK_CLIENT_ID'),
+        'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
+        'redirect' => 'FACEBOOK_REDIRECT_URL',
+    ],
+
+    'linkedin-openid' => [
+        'client_id' => env('LINKEDIN_CLIENT_ID'),
+        'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
+        'redirect' => 'LINKEDIN_REDIRECT_URL',
+    ],
+
 ];
