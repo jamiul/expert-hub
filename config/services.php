7845048f03c9e8502e16dccc8de24ff83597d58e
shamimhasan <shamimhasan524@yahoo.com>
2024-02-27 07:53:10 +0600

    Social Login

--- config/services.php
+++ config/services.php
@@ -34,13 +34,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' => route('auth.google.callback'),
+        'redirect' => env('GOOGLE_REDIRECT'),
     ],
 
     'linkedin-openid' => [
         'client_id' => env('LINKEDIN_CLIENT_ID'),
         'client_secret' => env('LINKEDIN_CLIENT_SECRET'),
-        'redirect' => route('auth.linkedin.callback'),
+        'redirect' => env('LINKEDIN_REDIRECT'),
     ],
 
 ];
